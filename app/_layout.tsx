10d7d812c8565257611bf67f77c34a6adba748f6
ronan <ronan.garrigues@etu.univ-amu.fr>
2025-03-28 19:29:24 +0100

    Ajout idée architecture v2

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,27 @@
-import { Stack } from "expo-router";
+import { Tabs } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
 
-export default function RootLayout() {
-  return <Stack />;
+export default function Layout() {
+  return (
+      <Tabs>
+        <Tabs.Screen
+            name="home"
+            options={{
+              title: "Accueil",
+              tabBarIcon: ({ color, size }) => (
+                  <Ionicons name="home" color={color} size={size} />
+              ),
+            }}
+        />
+        <Tabs.Screen
+            name="profile"
+            options={{
+              title: "Profil",
+              tabBarIcon: ({ color, size }) => (
+                  <Ionicons name="person" color={color} size={size} />
+              ),
+            }}
+        />
+      </Tabs>
+  );
 }
---
+++ app/profileScreen.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const ProfileScreen = () => {
+    return (
+        <View>
+            <Text>🎥 Mes films vus</Text>
+            <Text>📌 Watchlist</Text>
+            <Text>📊 Stats</Text>
+        </View>
+    );
+};
+
+export default ProfileScreen;
